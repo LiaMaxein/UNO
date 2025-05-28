a7b3fed9e4adce053c8b15d8f97c4968d64c9e7e
La-Donna <donna.rohm96@gmail.com>
2025-05-28 10:25:32 +0200

    second commit

--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,11 @@
 import java.util.List;
 
 public class Player {
-
     private String name;
     private List<Card> hand;
 
     public void playTurn(Card topCard) {
-        // Choose a playable card or draw
+
     }
 
 }
