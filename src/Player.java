03b2b2cb999214b486fa474d9f8b95e324fb1642
rachitamaniyar <rachitamaniyar@gmail.com>
2025-06-03 10:41:22 +0200

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,16 @@
 import java.util.List;
 
 public class Player {
+
     private String name;
-    private List<Card> hand;
-
-    public void playTurn(Card topCard) {
-
+    private int score;
+    private boolean isHuman;
+    private List<Card> hand = new ArrayList<>();
+    //COnstructor
+    public Player(String name, boolean isHuman) {
+        this.name = name;
+        this.isHuman = isHuman;
+        score = 0;
     }
 
 }
